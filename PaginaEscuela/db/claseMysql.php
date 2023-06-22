89c9b0d319234032658ab5f85f661b78b9daf8a9
Ciro <lopezciromartin@gmail.com>
2023-06-22 16:43:09 -0300

    Create claseMysql.php

---
+++ PaginaEscuela/db/claseMysql.php
@@ -0,0 +1,43 @@
+<?php /*Etiqueta de apertura, de nuestro codigo php */
+error_reporting(0);
+class DataBaseMysql{
+    var $BaseDatos;
+    var $Servidor;
+    var $Usuario;
+    var $Clave;
+
+    var $Conexion_ID = 0;
+
+    var $Error = "";
+
+    public function __construct($host = "localhost", $user = "root", $pass = "", $bd = "escuelatecnica2") {
+    
+        $this->Servidor = $host;
+        $this->Usuario = $user;
+        $this->Clave = $pass;
+        $this->BaseDatos = $bd;
+    }
+
+    /*Conexión a la base de datos*/
+
+    function conectar($host, $user, $pass, $bd){
+        if ($host != "") $this->Servidor = $host;
+        if ($user != "") $this->Usuario = $user;
+        if ($pass != "") $this->Clave = $pass;
+        if ($bd != "") $this->BaseDatos = $bd;
+        
+        // Conectamos al servidor
+        $this->Conexion_ID = mysqli_connect($this->Servidor, $this->Usuario, $this->Clave, $this->BaseDatos);
+        
+        if (!$this->Conexion_ID) {
+            $this->Error = "Ha fallado la conexión.";
+            return 0;
+        } 
+        
+        /* Si hemos tenido éxito conectando devuelve el identificador de la conexión, sino devuelve 0 */
+        return $this->Conexion_ID;
+    }
+}
+
+?> <!--Etiqueta de cierre de nuestro codigo php. -->
+
