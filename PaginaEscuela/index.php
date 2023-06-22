5c0919351701e85ce735bdabde0fe8a8c970fc16
Ciro <lopezciromartin@gmail.com>
2023-06-22 16:43:15 -0300

    Create index.php

---
+++ PaginaEscuela/index.php
@@ -0,0 +1,5 @@
+<?php
+//Utilizaremos codigo php, de esta manera lo indicamos
+require_once("controller/control.php");//Se esta indicando que se agregara el archivo control.php ubicado en la carpeta controllers
+
+?><!--Cerramos el codigo PHP-->
